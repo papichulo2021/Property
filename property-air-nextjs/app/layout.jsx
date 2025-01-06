6a64cb2c669b9cefce2afe2cc1413264b406eced
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-01-06 10:58:02 -0800

    Home Page & Property Card Component

--- property-air-nextjs/app/layout.jsx
+++ property-air-nextjs/app/layout.jsx
@@ -1,4 +1,6 @@
 import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata = {
     title: 'Property Air',
@@ -10,9 +12,11 @@ const MainLayout = ({children}) => {
     return (
         <html>
             <body>
+                <Navbar />
                 <main>
                     {children}
                 </main>
+                <Footer />
             </body>
         </html>
     );
---
+++ property-air-nextjs/app/properties/[id]/page.jsx
@@ -0,0 +1,11 @@
+
+const PropertyPage = ({ params,  }) => {
+    
+    return ( 
+        <div>
+            Property Page {params.name}
+        </div>
+     );
+}
+ 
+export default PropertyPage
