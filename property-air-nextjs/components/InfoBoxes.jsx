b4e71eebc6bbdb22c5f55656024dfda4fec1ff8d
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-01-06 11:53:51 -0800

    Home Property Listings

--- property-air-nextjs/components/InfoBoxes.jsx
+++ property-air-nextjs/components/InfoBoxes.jsx
@@ -12,12 +12,14 @@ const InfoBoxes = () => {
             }}>
                 Find your dream rental property. Bookmark properties and contact owners
             </InfoBox>
-            <InfoBox heading="For Property Owners" backgroundColor="bg-blue-100"
-            buttonInfo={{
-                text: 'Add Property',
-                link: '/properties/add',
-                backgroundColor: 'bg-blue-500'
-            }}>
+            <InfoBox 
+                heading="For Property Owners" 
+                backgroundColor="bg-blue-100"
+                buttonInfo={{
+                    text: 'Add Property',
+                    link: '/properties/add',
+                    backgroundColor: 'bg-blue-500'
+                }}>
                 List your properties and reach potential tenants. Rent as an airbnb 
                 or long term
             </InfoBox>
