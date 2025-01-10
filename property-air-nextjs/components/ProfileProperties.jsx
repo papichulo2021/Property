23e376cdb22aab771edb26ff91cf01a969f20fa9
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-01-09 16:27:12 -0800

    Deleting Properties

--- property-air-nextjs/components/ProfileProperties.jsx
+++ property-air-nextjs/components/ProfileProperties.jsx
@@ -2,10 +2,23 @@
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
+import deleteProperty from "@/app/actions/deleteProperty";
 
 const ProfileProperties = ({ properties:initialProperties } ) => {
     const [properties, setProperties] = useState(initialProperties);
 
+    const handleDeleteProperty = async (propertyId) => {
+        const confirmed = window.confirm('are you sure you want to delete this property?');
+        
+        if(!confirmed) return;
+
+        await deleteProperty(propertyId);
+
+        const updatedProperties = properties.filter((property) => property._id !==
+        propertyId);
+
+        setProperties(updatedProperties)
+    }
 
     return properties.map((property) => (
         <div key={property._id} className="mb-10">
@@ -33,6 +46,7 @@ const ProfileProperties = ({ properties:initialProperties } ) => {
                   <button
                     className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
                     type="button"
+                    onClick={ ()=> handleDeleteProperty(property._id)}
                   >
                     Delete
                   </button>
