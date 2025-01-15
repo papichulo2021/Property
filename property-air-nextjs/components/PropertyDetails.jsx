47a55773f9a94a56e8720cc56b5e15534b4369a6
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-01-14 16:03:10 -0800

    CheckBookmark Status

--- property-air-nextjs/components/PropertyDetails.jsx
+++ property-air-nextjs/components/PropertyDetails.jsx
@@ -91,10 +91,6 @@ const PropertyDetails = ( {property} ) => {
                  ))}
               </ul>
             </div>
-            {/* <!-- Map --> */}
-            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-              <div id="map"></div>
-            </div>
           </main>
         </>
      );
